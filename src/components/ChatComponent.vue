5c1d64a490a6e5e9fb442a5a5dba236b837cd3ea
Sinthix <ivo.ivan89@hotmail.com>
2024-09-14 10:53:18 +0100

    feat: adds conversation and compose area first part

--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -1,19 +1,39 @@
 <template>
     <div class="chat-area">
-        hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?
-        hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corrupti odit quasi fuga aperiam nesciunt ipsam. Quo earum nesciunt, fuga mollitia quos recusandae cum rerum, impedit voluptatum alias eius quas?
+      <div class="conversation-area">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, minima quaerat. Illum perspiciatis alias vitae quas inventore sunt earum delectus aliquam iste. Dolorem inventore autem rem distinctio quas, atque iste.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, minima quaerat. Illum perspiciatis alias vitae quas inventore sunt earum delectus aliquam iste. Dolorem inventore autem rem distinctio quas, atque iste.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, minima quaerat. Illum perspiciatis alias vitae quas inventore sunt earum delectus aliquam iste. Dolorem inventore autem rem distinctio quas, atque iste.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, minima quaerat. Illum perspiciatis alias vitae quas inventore sunt earum delectus aliquam iste. Dolorem inventore autem rem distinctio quas, atque iste.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, minima quaerat. Illum perspiciatis alias vitae quas inventore sunt earum delectus aliquam iste. Dolorem inventore autem rem distinctio quas, atque iste.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, minima quaerat. Illum perspiciatis alias vitae quas inventore sunt earum delectus aliquam iste. Dolorem inventore autem rem distinctio quas, atque iste.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, minima quaerat. Illum perspiciatis alias vitae quas inventore sunt earum delectus aliquam iste. Dolorem inventore autem rem distinctio quas, atque iste.
+      </div>
+      <div class="compose-area">
+        <input type="text">
+        <button>
+            s
+        </button>
+      </div>
     </div>
 </template>
 
 <style scoped>
 .chat-area {
-    display: grid;
     width: 500px;
-    height: 400px;
-    max-height: 400px;
+    height: 300px;
     border: 1px solid black;
+}
+.conversation-area {
+    width: 100%;
+    max-height: 300px;
     overflow: scroll;
     scrollbar-color: red orange;
     scrollbar-width: thin;
+}
+.compose-area {
+    display: grid;
+    grid-template-columns: 1fr 60px;
+    grid-gap: 12px;
 }
 </style>
